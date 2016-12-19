fb6900015a0173f0a5534b19e9b170a8a7ad3dd1
Nigel Horne <njh@bandsman.co.uk>
2016-12-19 08:49:04 -0500

    Started to build test suite

--- lib/Geo/Coder/GooglePlaces.pm
+++ lib/Geo/Coder/GooglePlaces.pm
@@ -23,13 +23,7 @@ Geo::Coder::GooglePlaces - Google Maps Geocoding API
 
 Geo::Coder::GooglePlaces provides a geocoding functionality using Google Maps API.
 
-See L<Geo::Coder::GooglePlaces::V2> for V2 API usage.
-
 See L<Geo::Coder::GooglePlaces::V3> for V3 API usage.
-
-B<Note that Google no longer supports the V2 API. Geo::Coder::GooglePlaces defaults 
-to the V3 API. The V2 interface is still here but any attempts to use it will
-fail since the V2 API service is no longer reachable.>
 
 =head1 LICENSE
 
